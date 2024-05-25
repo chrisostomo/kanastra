3862fed7ab0cd6e510cfcfb0ea9eaf7a3f5378bf
Bruno Chrisostomo <bruno.chrisostmo@gmail.com>
2024-05-25 07:13:51 -0300

    Ajustada estrutura

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,19 +1,16 @@
-from fastapi import FastAPI, BackgroundTasks, HTTPException
+from fastapi import FastAPI, File, Form, UploadFile, BackgroundTasks, HTTPException
 from .tasks import process_csv_task, save_file
-from .schemas import ProcessCSVRequest, TasksResponse
+from .schemas import TasksResponse
 from .redis_client import RedisClient
 
 app = FastAPI()
 redis_client = RedisClient()
 
 @app.post("/upload_csv/")
-async def upload_csv(request: ProcessCSVRequest, background_tasks: BackgroundTasks):
-    email = request.email
-    csv_data = request.csv_data
-
+async def upload_csv(background_tasks: BackgroundTasks, email: str = Form(...), file: UploadFile = File(...)):
     try:
-        # Save file to disk
-        file_path = save_file(csv_data.encode(), f"{email}.csv")
+        file_content = await file.read()
+        file_path = save_file(file_content, file.filename)
 
         # Store the task in Redis as 'processing'
         task_id = redis_client.create_task(email)
--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,26 +1,9 @@
 from pydantic import BaseModel
 from typing import Dict
 
-class ProcessCSVRequest(BaseModel):
-    email: str
-    csv_data: str
-
 class TaskStatus(BaseModel):
-    """
-    Model representing the status of a task.
-
-    Attributes:
-        task_id (str): The unique identifier of the task.
-        status (str): The status of the task (e.g., 'processing', 'completed', 'failed').
-    """
     task_id: str
     status: str
 
 class TasksResponse(BaseModel):
-    """
-    Model representing a response with multiple task statuses.
-
-    Attributes:
-        tasks (Dict[str, str]): A dictionary mapping task IDs to their statuses.
-    """
     tasks: Dict[str, str]
