39f9e2b2b1ff07e64d8ac2ae1693cea67e9cf952
Bruno Chrisostomo <bruno.chrisostmo@gmail.com>
2024-05-25 09:15:53 -0300

    ajusta rota

--- frontend/src/pages/Index/Index.tsx
+++ frontend/src/pages/Index/Index.tsx
@@ -5,9 +5,8 @@ import FileList from '../../components/FileList.tsx';
 const Index = () => {
   return (
     <div className="flex flex-col items-center p-4">
-      aaaaaa
-      {/*<FileUploader />*/}
-      {/*<FileList />*/}
+      <FileUploader />
+      <FileList />
     </div>
   );
 };
